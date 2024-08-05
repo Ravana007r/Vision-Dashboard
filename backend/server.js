8459ea4968e996de65728457f5297545dc0655cb
Akash Kumar <128532440+Ravana007r@users.noreply.github.com>
2024-08-06 01:40:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,14 @@ const { Server } = require("socket.io");
 
 const { addUser, getUser, removeUser } = require("./utils/users");
 
-// const { PeerServer } = require("peer");
+const peerServer = PeerServer({
+  host:"/",
+  port: 443,
+  path: "/",
+  secure: true
+});
 
-// const peerServer = PeerServer({
-//   host:"/".
-//   port: 5001,
-//   path: "/",
-// });
-
-// app.use(peerServer);
+app.use(peerServer);
 
 const io = new Server(server);
 // server.on("upgrade", (request, socket, head) => {});
